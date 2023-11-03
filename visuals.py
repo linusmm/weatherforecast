c77834b81ee0d56f0e1e876d05fcc62ddcb5805f
Linus Marheineke <linus.marheineke@etu.umontpellier.fr>
2023-11-03 18:47:51 +0100

    changes

--- visuals.py
+++ visuals.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
+
+weekdays_dict = {
+  1:"Monday", 
+  2:"Tuesday", 
+  3:"Wednesday",
+  4:"Thursday",
+  5:"Friday",
+  6:"Saturday",
+  7:"Sunday"}
 
 
 def get_sky(weather: str):
@@ -9,3 +20,16 @@ def get_sky(weather: str):
         return "http://www.clker.com/cliparts/4/2/0/7/11949892071155302510rain.svg"
     else:
         return None
+
+
+def get_weekday(daily_data):
+    """ takes daily data and returns the respective weekday"""
+    return weekdays_dict[datetime.strptime(daily_data, '%Y-%m-%d').weekday()]
+
+
+def create_MD_weather_table(data):
+    """takes data and creates markdown tables displaying it"""
+    table = f"""
+    | Today | {get_weekday(data['daily']['time'][])}
+    """
+    return
